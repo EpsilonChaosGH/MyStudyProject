e4aba406aeca4a412d59e9a767bcd5f2a0bf6d4c
Artem Morozov <darkness256i@gmail.com>
2022-07-27 16:34:03 +0300

    single choice AlertDialog

---
+++ app/src/main/java/com/example/mystudyproject/DialogsFragment.kt
@@ -0,0 +1,101 @@
+package com.example.mystudyproject
+
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
+import com.example.mystudyproject.databinding.FragmentDialogsBinding
+
+class DialogsFragment: Fragment() {
+    private lateinit var binding: FragmentDialogsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentDialogsBinding.inflate(inflater, container, false)
+
+
+
+        val listener = DialogInterface.OnClickListener { _, v ->
+            when (v) {
+                DialogInterface.BUTTON_POSITIVE -> toast("positive")
+                DialogInterface.BUTTON_NEGATIVE -> toast("negative")
+                DialogInterface.BUTTON_NEUTRAL -> toast("neutral")
+                else -> toast("oops")
+            }
+        }
+        val dialog = AlertDialog.Builder(requireContext())
+            .setIcon(R.mipmap.ic_launcher_round)
+            .setTitle("title")
+            .setMessage("message")
+            .setPositiveButton("positive", listener)
+            .setNegativeButton("negative", listener)
+            .setNeutralButton("neutral", listener)
+            .setOnCancelListener {
+                toast("cancel")
+            }
+            .create()
+
+        binding.button2.setOnClickListener {
+            dialog.show()
+        }
+
+
+
+
+
+
+            class ValuesDialog2(
+                var values: List<Int>,
+                var currentIndex: Int
+            )
+
+            fun createValues(currentValues: Int): ValuesDialog2 {
+                val values = 0..100 step 10
+                val currentIndex = values.indexOf(currentValues)
+                return if (currentIndex == -1) {
+                    val list = values + currentValues
+                    ValuesDialog2(list, list.lastIndex)
+                } else {
+                    ValuesDialog2(values.toList(), currentIndex)
+                }
+            }
+
+            var vl = 5
+
+            fun singleChoiceAlertDialog() {
+
+                val items = createValues(vl)
+                val textItem = items.values
+                    .map { "value = $it" }
+                    .toTypedArray()
+
+                val dialog2 = AlertDialog.Builder(requireContext())
+                    .setTitle("title")
+                    .setSingleChoiceItems(textItem, items.currentIndex) { dialog, witch ->
+                        vl = items.values[witch]
+                        dialog.dismiss()
+                        binding.currentValue.text = vl.toString()
+                    }
+                    .create()
+                dialog2.show()
+            }
+
+            binding.currentValue.text = vl.toString()
+            binding.btDialogSingleChoice.setOnClickListener {
+                singleChoiceAlertDialog()
+            }
+
+            return binding.root
+        }
+
+    private fun toast(text: String) {
+        Toast.makeText(requireContext(), text, Toast.LENGTH_SHORT).show()
+    }
+}
--- app/src/main/java/com/example/mystudyproject/MainActivity.kt
+++ app/src/main/java/com/example/mystudyproject/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.Fragment
 import com.example.mystudyproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,30 +17,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val listener = DialogInterface.OnClickListener { _, v ->
-            when (v) {
-                DialogInterface.BUTTON_POSITIVE -> toast("positive")
-                DialogInterface.BUTTON_NEGATIVE -> toast("negative")
-                DialogInterface.BUTTON_NEUTRAL -> toast("neutral")
-                else -> toast("oops")
-            }
+        if (savedInstanceState == null){
+            val fragment = Fragment()
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragmentContainerView, fragment)
+                .commit()
+
+
         }
-        val dialog = AlertDialog.Builder(this)
-            .setIcon(R.mipmap.ic_launcher_round)
-            .setTitle("title")
-            .setMessage("message")
-            .setPositiveButton("positive", listener)
-            .setNegativeButton("negative", listener)
-            .setNeutralButton("neutral", listener)
-            .setOnCancelListener{
-                toast("cancel")
-            }
-            .create()
-        dialog.show()
+
 
     }
 
-    private fun toast(text: String) {
-        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
-    }
 }
