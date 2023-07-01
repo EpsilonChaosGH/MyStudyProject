4c50dc1465c816305fb761123f37d2ffb37156a2
EpsilonChaos <DARKNESS256I@GMAIL.COM>
2023-07-01 19:15:12 +0300

    Add recursiveBinarySearch and quickSort

--- app/src/main/java/com/example/mystudyproject/algorithms/algorithm.kt
+++ app/src/main/java/com/example/mystudyproject/algorithms/algorithm.kt
@@ -3,40 +3,50 @@ package com.example.mystudyproject.algorithms
 import java.util.*
 
 fun main() {
+
     val numbers = listOf(12, 11, 12, -32, 0, -2, 6, 23)
 
     selectionSort(numbers)
-
-    //var isSorted = false
-
-//    while (!isSorted) {
-//        isSorted = true
-//        for (i in 1 until numbers.size) {
-//            if (numbers[i] < numbers[i - 1]) {
-//                Collections.swap(numbers, i, i - 1)
-//                isSorted = false
-//            }
-//        }
-//    }
-//
-//    for (i in numbers.indices) {
-//        var minIndex = i
-//        for (x in i + 1 until numbers.size) if (numbers[minIndex] > numbers[x]) minIndex = x
-//        if (numbers[minIndex] != numbers[i]) {
-//            Collections.swap(numbers, minIndex, i)
-//        }
-//    }
-//
-//    println(numbers)
+    bubbleSort(numbers)
+    println(quickSort(numbers))
 }
 
 fun selectionSort(list: List<Int>) {
     for (i in list.indices) {
         var minIndex = i
-        for (x in i + 1 until list.size) if (list[minIndex] > list[x]) minIndex = x
-        if (list[minIndex] != list[i]) {
-            Collections.swap(list, minIndex, i)
+        for (x in i + 1 until list.size) {
+            if (list[minIndex] > list[x]) minIndex = x
+        }
+        Collections.swap(list, i, minIndex)
+    }
+    println(list)
+}
+
+fun bubbleSort(list: List<Int>) {
+    var isSorted = false
+    while (!isSorted) {
+        isSorted = true
+        for (i in 1 until list.size) {
+            if (list[i] < list[i - 1]) {
+                Collections.swap(list, i, i - 1)
+                isSorted = false
+            }
         }
     }
-    println("Selection Sort: $list")
+    println(list)
+}
+
+fun quickSort(list: List<Int>): List<Int> {
+    if (list.size <= 1) return list
+    val pivotIndex = list.size / 2
+    val pivot = list[pivotIndex]
+    val less = mutableListOf<Int>()
+    val greater = mutableListOf<Int>()
+
+    for (i in list.indices) {
+        if (i == pivotIndex) continue
+        if (list[i] < pivot) less.add(list[i])
+        else greater.add(list[i])
+    }
+    return quickSort(less) + pivot + quickSort(greater)
 }
